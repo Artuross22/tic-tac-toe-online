2d0bd80d624cdd15f475c8f448a93b8c863f0cf6
Artuross22 <bonjjjuaro@gmail.com>
2023-12-10 22:37:46 +0200

    init next project and the beginning of work with tic-tac-toe

---
+++ components/game/game-info.jsx
@@ -0,0 +1,26 @@
+import { GameSymbol } from "./game-symbol"
+import styles from './game.module.css'
+
+export function GameInfo({ isDraw, winnerSymbol, currentStep }) {
+  if (isDraw) {
+    return (
+      <div className={styles["game-info"]}>
+        Draw
+      </div>
+    )
+  }
+
+  if (winnerSymbol) {
+    return (
+      <div className={styles["game-info"]}>
+        Winner: <GameSymbol symbol={winnerSymbol} />
+      </div>
+    )
+  }
+
+  return (
+    <div className={styles["game-info"]}>
+      Turn: <GameSymbol symbol={currentStep} />
+    </div>
+  )
+}
