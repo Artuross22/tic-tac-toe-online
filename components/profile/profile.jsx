02040d66269cfadfa409039d254814018e1bd4ed
Artuross22 <bonjjjuaro@gmail.com>
2023-12-30 18:07:20 +0200

    Added game info data display

--- components/profile/profile.jsx
+++ components/profile/profile.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import avatarSrc from "./avatar.png";
 import clsx from "clsx";
 
-export function Profile({ className }) {
+export function Profile({ className, name, rating, avatar = avatarSrc }) {
   return (
     <div
       className={clsx(
@@ -10,10 +10,12 @@ export function Profile({ className }) {
         className,
       )}
     >
-      <Image src={avatarSrc} width={48} height={48} alt="avatar" unoptimized />
-      <div>
-        <div className=" text-lg leading-tight">Artur</div>
-        <div className="text-slate-400 text-xs leading-tight">rating: 888</div>
+      <Image src={avatar} width={48} height={48} alt="avatar" unoptimized />
+      <div className="overflow-hidden">
+        <div className=" text-lg leading-tight truncate ">{name}</div>
+        <div className="text-slate-400 text-xs leading-tight">
+          Рейтинг: {rating}
+        </div>
       </div>
     </div>
   );
