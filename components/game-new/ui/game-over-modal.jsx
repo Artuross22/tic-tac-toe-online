5aa0921b3f565b2cf7049aeeddf5e86a962e7b19
Artuross22 <bonjjjuaro@gmail.com>
2024-01-14 20:09:07 +0200

    refactoring

--- components/game-new/ui/game-over-modal.jsx
+++ components/game-new/ui/game-over-modal.jsx
@@ -1,17 +1,20 @@
-import { UiButton } from "../uikit/ui-button";
-import { UiModal } from "../uikit/ui-modal";
+import { UiButton } from "../../uikit/ui-button";
+import { UiModal } from "../../uikit/ui-modal";
 
-export function GameOverModal() {
+export function GameOverModal({ winnerName, players }) {
   return (
     <UiModal
       width="md"
-      isOpen={winnerSymbol}
+      isOpen={winnerName}
       onClose={() => console.log("close")}
     >
       <UiModal.Header>Game over</UiModal.Header>
       <UiModal.Body>
         <div className="text-sm">
-          Winner: <span className="text-teal-600">Artur</span>
+          Winner: <span className="text-teal-600">{winnerName}</span>
+        </div>
+        <div className="justify-between grid grid-cols-2 gap-3 mt-2">
+          {players}
         </div>
       </UiModal.Body>
       <UiModal.Footer>
